d03f3c24aaaced27bceebe5b0ac5219a37c1265a
junicotandiago198 <junicotandiagoo21@gmail.com>
2023-01-19 22:49:07 +0700

    add and custom authentication login page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect()->route('login'));
 
 Route::middleware([
     'auth:sanctum',
